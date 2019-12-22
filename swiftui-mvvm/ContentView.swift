8503151fcbf946ccf4079bb5da65bab381dd024d
Md Al Mamun <md.almamun@loblaw.ca>
2019-12-22 10:57:59 -0500

    Initial Commit

---
+++ swiftui-mvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mvvm
+//
+//  Created by MD AL MAMUN (LCL) on 2019-12-22.
+//  Copyright © 2019 MD AL MAMUN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-mvvmTests/swiftui_mvvmTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_mvvmTests.swift
+//  swiftui-mvvmTests
+//
+//  Created by MD AL MAMUN (LCL) on 2019-12-22.
+//  Copyright © 2019 MD AL MAMUN. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_mvvm
+
+class swiftui_mvvmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
